9e54e4d4dfd086d559675c9f6174c647db18dafe
BillZid <45631691+BillZid@users.noreply.github.com>
2023-07-13 12:38:41 +0800

    Update code.py

--- code.py
+++ code.py
@@ -1,52 +1,50 @@
-import os
 import logging
+import requests
 from qcloud_cos import CosConfig
 from qcloud_cos import CosS3Client
 
 class VideoPreview:
-    def __init__(self, secret_id, secret_key, bucket):
+    def __init__(self, secret_id: str, secret_key: str, region: str, bucket: str):
         self.secret_id = secret_id
         self.secret_key = secret_key
+        self.region = region
         self.bucket = bucket
 
-        # 配置腾讯云COS
-        region = 'ap-guangzhou'  # 根据你的实际情况进行修改
-        config = CosConfig(Region=region, SecretId=self.secret_id, SecretKey=self.secret_key)
+        # 配置日志
+        logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+
+        # 配置腾讯云存储桶
+        config = CosConfig(Region=self.region, SecretId=self.secret_id, SecretKey=self.secret_key)
         self.cos_client = CosS3Client(config)
 
-        # 配置日志记录
-        logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
-    def set_image_preview(self, image_id, preview_url):
+    def set_image_preview(self, image_id: str, preview_url: str) -> str:
         try:
-            # 将预览URL存储到腾讯云COS桶中
-            object_key = f"{image_id}.jpg"  # 假设以图片ID作为对象键
+            # 从预览URL获取图片内容
+            response = requests.get(preview_url)
+            response.raise_for_status()
+            image_data = response.content
+
+            # 保存图片到腾讯云存储桶
+            object_key = f"{image_id}.png"
             response = self.cos_client.put_object(
                 Bucket=self.bucket,
-                Body=preview_url,
-                Key=object_key
+                Body=image_data,
+                Key=object_key,
+                ContentType='image/png'
             )
-            if response['Response']['Error']['Code'] == 0:
-                logging.info(f"预览URL已成功存储到腾讯云COS桶中：{object_key}")
-            else:
-                logging.error(f"存储预览URL到腾讯云COS桶中时出错：{response['Response']['Error']['Message']}")
-        except Exception as e:
-            logging.error(f"存储预览URL到腾讯云COS桶中时出现异常：{str(e)}")
+            response.raise_for_status()
 
-    def get_image_preview(self, image_id):
-        try:
-            # 从腾讯云COS桶中获取与图片ID对应的预览URL
-            object_key = f"{image_id}.jpg"  # 假设以图片ID作为对象键
-            response = self.cos_client.get_object(
-                Bucket=self.bucket,
-                Key=object_key
-            )
-            if response['Response']['Error']['Code'] == 0:
-                preview_url = response['Body'].get_raw_stream().url
-                return preview_url
-            else:
-                logging.error(f"从腾讯云COS桶中获取预览URL时出错：{response['Response']['Error']['Message']}")
-                return None
+            # 返回保存在COS桶中的图片地址
+            return f"https://{self.bucket}.cos.{self.region}.myqcloud.com/{object_key}"
         except Exception as e:
-            logging.error(f"从腾讯云COS桶中获取预览URL时出现异常：{str(e)}")
-            return None
+            logging.error(f"Failed to set image preview for image_id {image_id}: {str(e)}")
+            return ""
+
+    def get_image_preview(self, image_id: str) -> str:
+        try:
+            # 返回保存在COS桶中的图片地址
+            object_key = f"{image_id}.png"
+            return f"https://{self.bucket}.cos.{self.region}.myqcloud.com/{object_key}"
+        except Exception as e:
+            logging.error(f"Failed to get image preview for image_id {image_id}: {str(e)}")
+            return ""
